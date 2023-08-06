f924b85f22ea92c29f16dbea208c297a9f54db9f
Ezejanu <Ezejanu_nno@ymail.com>
2023-08-06 11:24:00 -0700

    first test

---
+++ factors.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+import time
+
+def factor(n):
+    a = (int)(n / 2)
+    for i in range(2, a + 1):
+        if n % i ==  0:
+            p = i
+            q = n // p
+            print("{:d}={:d}*{:d}".format(n, p, q))
+            return
+    return
